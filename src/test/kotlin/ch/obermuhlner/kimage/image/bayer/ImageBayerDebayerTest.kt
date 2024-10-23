77ac56f99e590b3507719768eab857016997015c
Eric Obermühlner <eobermuhlner@gmail.com>
2024-10-23 12:50:30 +0200

    refactor to use assertReferenceImage

--- src/test/kotlin/ch/obermuhlner/kimage/image/bayer/ImageBayerDebayerTest.kt
+++ src/test/kotlin/ch/obermuhlner/kimage/image/bayer/ImageBayerDebayerTest.kt
@@ -10,37 +10,49 @@ import ch.obermuhlner.kimage.core.image.bayer.findBayerBadPixels
 import ch.obermuhlner.kimage.core.image.io.ImageWriter
 import ch.obermuhlner.kimage.core.matrix.DoubleMatrix
 import ch.obermuhlner.kimage.core.matrix.values.asXY
+import ch.obermuhlner.kimage.image.AbstractImageProcessingTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.io.File
 
-class ImageBayerDebayerTest {
+class ImageBayerDebayerTest: AbstractImageProcessingTest() {
 
     @Test
-    fun `should bayer image`() {
+    fun `should bayer then debayer an image`() {
         val width = 50
         val height = 30
 
         val image = createTestImage(width, height)
-        ImageWriter.write(image, File("before_bayer.tif"))
+
+        assertReferenceImage("before_bayer", image)
 
         val bayeredImage = image.bayer()
-        val stuckX = 2
-        val stuckY = 2
-        bayeredImage[Channel.Red].asXY()[stuckX, stuckY] = 1.0
-        bayeredImage[Channel.Green].asXY()[stuckX, stuckY] = 1.0
-        bayeredImage[Channel.Blue].asXY()[stuckX, stuckY] = 1.0
+
+        fun simulateStuckPixel(stuckX: Int, stuckY: Int, value: Double) {
+            bayeredImage[Channel.Red].asXY()[stuckX, stuckY] = value
+            bayeredImage[Channel.Green].asXY()[stuckX, stuckY] = value
+            bayeredImage[Channel.Blue].asXY()[stuckX, stuckY] = value
+        }
+
+        simulateStuckPixel(2, 2, 1.0)
+        simulateStuckPixel(5, 3, 0.0)
+        simulateStuckPixel(width-5, 2, 1.0)
         ImageWriter.write(bayeredImage, File("after_bayer.tif"))
 
         val bayeredColorImage = bayeredImage.debayer(interpolation = DebayerInterpolation.None)
-        ImageWriter.write(bayeredColorImage, File("after_bayer_color.tif"))
+        assertReferenceImage("after_bayer_color", bayeredColorImage)
 
         val stuckPixels = bayeredImage[Channel.Red].findBayerBadPixels()
         println(stuckPixels)
-        assertEquals(setOf(Pair(2,2)), stuckPixels)
+        assertEquals(
+            setOf(
+                Pair(2,2),
+                Pair(5,3),
+                Pair(width-5, 2)
+            ), stuckPixels)
 
         val debayeredImage = bayeredImage.debayer(badpixelCoords = stuckPixels)
-        ImageWriter.write(debayeredImage, File("after_debayer.tif"))
+        assertReferenceImage("after_debayer", debayeredImage)
 
         assertEquals(setOf<Pair<Int,Int>>(), debayeredImage.bayer()[Channel.Red].findBayerBadPixels())
     }
