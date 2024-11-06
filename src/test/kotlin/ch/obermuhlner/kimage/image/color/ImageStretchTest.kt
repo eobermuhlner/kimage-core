0ff0b0aa7394196f1fcbd8e1abc1174a7db11e04
Eric Obermühlner <eobermuhlner@gmail.com>
2024-11-06 21:33:31 +0100

    enable stretch tests

--- src/test/kotlin/ch/obermuhlner/kimage/image/color/ImageStretchTest.kt
+++ src/test/kotlin/ch/obermuhlner/kimage/image/color/ImageStretchTest.kt
@@ -21,16 +21,16 @@ class ImageStretchTest : AbstractImageProcessingTest() {
     fun testStretch() {
         val image = readTestImage("small_M42.png")
 
-//        assertReferenceImage("stretchLinear", image.stretchLinear())
-//        assertReferenceImage("stretchLinearPercentile", image.stretchLinearPercentile())
-//        assertReferenceImage("stretchLinearFactor", image.stretchLinearFactor(2.0))
-//        assertReferenceImage("stretchAsinh", image.stretchAsinh())
-//        assertReferenceImage("stretchAsinhPercentile", image.stretchAsinhPercentile())
-//        assertReferenceImage("stretchExponential", image.stretchExponential(0.1))
-//        assertReferenceImage("stretchExponentialPercentile", image.stretchExponentialPercentile())
-//        assertReferenceImage("stretchExponentialMedian", image.stretchExponentialMedian())
+        assertReferenceImage("stretchLinear", image.stretchLinear())
+        assertReferenceImage("stretchLinearPercentile", image.stretchLinearPercentile())
+        assertReferenceImage("stretchLinearFactor", image.stretchLinearFactor(2.0))
+        assertReferenceImage("stretchAsinh", image.stretchAsinh())
+        assertReferenceImage("stretchAsinhPercentile", image.stretchAsinhPercentile())
+        assertReferenceImage("stretchExponential", image.stretchExponential(0.1))
+        assertReferenceImage("stretchExponentialPercentile", image.stretchExponentialPercentile())
+        assertReferenceImage("stretchExponentialMedian", image.stretchExponentialMedian())
         assertReferenceImage("stretchSigmoid", image.stretchSigmoid())
-//        assertReferenceImage("stretchLogarithmic", image.stretchLogarithmic())
-//        assertReferenceImage("stretchSTF", image.stretchSTF())
+        assertReferenceImage("stretchLogarithmic", image.stretchLogarithmic())
+        assertReferenceImage("stretchSTF", image.stretchSTF())
     }
 }
