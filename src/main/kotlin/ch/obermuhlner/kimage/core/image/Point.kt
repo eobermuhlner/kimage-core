bc40af1178f59090f448451f696c83015439e440
Eric Obermühlner <eobermuhlner@gmail.com>
2024-11-06 21:34:06 +0100

    remove old Point file

--- src/main/kotlin/ch/obermuhlner/kimage/core/image/Point.kt
+++
@@ -1,9 +0,0 @@
-package ch.obermuhlner.kimage.core.image
-
-data class PointXY(val x: Int, val y: Int) {
-    fun toPointRowCol() = PointRowCol(y, x)
-}
-
-data class PointRowCol(val row: Int, val col: Int) {
-    fun toPointXY() = PointXY(col, row)
-}
